e289d6280654705caa4c73450479fc9addcee01f
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-28 15:36:12 -0500

    first commit

---
+++ src/test/java/com/example/fitnessapi/FitnessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitnessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
