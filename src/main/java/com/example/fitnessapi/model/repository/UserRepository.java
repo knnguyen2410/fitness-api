8bb8a5d9519060dd1a9301c4b6b32913ba1e5507
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 12:36:58 -0500

    update docstrings

--- src/main/java/com/example/fitnessapi/model/repository/UserRepository.java
+++ src/main/java/com/example/fitnessapi/model/repository/UserRepository.java
@@ -4,11 +4,10 @@ import com.example.fitnessapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // user register
-
     /**
      * existsByEmailAddress is a custom method that
      * checks if a user already exists by their email address.
+     * This step happens when the user registers.
      * @param emailAddress is what we're looking up the user by
      * @return true if user exists, false if user does not exist
      */
@@ -17,9 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /**
      * findUserByEmailAddress is a custom method that
      * finds and returns us the user from an email address.
+     * This step happens when the user logins.
      * @param emailAddress is what we're looking up the user by
      * @return the User from the emailAddress
      */
-    // user login
     User findUserByEmailAddress(String emailAddress);
 }
