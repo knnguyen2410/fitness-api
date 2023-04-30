27af7f37aa5f806854e645b8032567d81f24f4b8
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 12:25:57 -0500

    add body for LoginResponse class

--- src/main/java/com/example/fitnessapi/model/response/LoginResponse.java
+++ src/main/java/com/example/fitnessapi/model/response/LoginResponse.java
@@ -1,4 +1,20 @@
 package com.example.fitnessapi.model.response;
 
 public class LoginResponse {
+    // this is the response message that the user sees after a login attempt
+    private String message;
+
+    // constructor
+    public LoginResponse(String message){
+        this.message = message;
+    }
+
+    // getters and setters
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
 }
