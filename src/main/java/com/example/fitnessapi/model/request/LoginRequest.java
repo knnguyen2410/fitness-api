f362d75bcac9a93f4de725365b1f775ca4e442a5
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 12:10:15 -0500

    instantiate class variables for user login request

--- src/main/java/com/example/fitnessapi/model/request/LoginRequest.java
+++ src/main/java/com/example/fitnessapi/model/request/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.example.fitnessapi.model.request;
 
 public class LoginRequest {
+    // a user uses a userName and password to login
+    private String userName;
+    private String password;
+
+    // create getters only since we are not setting these
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
 }
