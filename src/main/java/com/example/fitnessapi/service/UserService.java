4aae1dbe8debfc201de4a48e4226ce9be3b2a9ea
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:37:30 -0500

    add method to find user from email address

--- src/main/java/com/example/fitnessapi/service/UserService.java
+++ src/main/java/com/example/fitnessapi/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.model.User;
 import com.example.fitnessapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,15 @@ public class UserService {
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    /**
+     * findUserByEmailAddress returns a user from their email address
+     * @param emailAddress is used to find the user
+     * @return the user
+     */
+    public User findUserByEmailAddress(String emailAddress) {
+        return userRepository.findUserByEmailAddress(emailAddress);
     }
 
 // As a user, I can register for an account using my email address, and set a username and password.
