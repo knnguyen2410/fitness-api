e620e7168da7d43395ba6946093f7a44a2528b2c
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:41:09 -0500

    add docstring

--- src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
@@ -18,6 +18,13 @@ public class MyUserDetailsService implements UserDetailsService {
         this.userService = userService;
     }
 
+    /**
+     * This is the method that comes from the UserDetailsService interface"
+     * loadUserByUsername returns a user using the email address, or throws an exception if email address not found
+     * @param emailAddress is used to find the user
+     * @return the user
+     * @throws UsernameNotFoundException if email address not found
+     */
     @Override
     public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
         User user = userService.findUserByEmailAddress(emailAddress);
