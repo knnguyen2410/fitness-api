da3d32cef3588862c4700a4a54d626ed701d8c1b
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 23:38:09 -0500

    instantiate workout service in workout controller

--- src/main/java/com/example/fitnessapi/controller/WorkoutController.java
+++ src/main/java/com/example/fitnessapi/controller/WorkoutController.java
@@ -1,5 +1,7 @@
 package com.example.fitnessapi.controller;
 
+import com.example.fitnessapi.service.WorkoutService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/") // this is the designated url path: http://localhost:9092/api/
 public class WorkoutController {
 
-// http://localhost:9092/api/workouts/
+    private WorkoutService workoutService;
+
+    @Autowired
+    public void setWorkoutService(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
+
+    // http://localhost:9092/api/workouts/
 // (POST) As a user, I can create a new workout
 // http://localhost:9092/api/workouts/
 // (GET) As a user, I can see a list of all my workouts
