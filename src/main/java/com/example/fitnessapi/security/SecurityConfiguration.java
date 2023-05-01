c3aeed75921e03c376a34736cac1c31ba592fc1d
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:54:27 -0500

    add docstrings

--- src/main/java/com/example/fitnessapi/security/SecurityConfiguration.java
+++ src/main/java/com/example/fitnessapi/security/SecurityConfiguration.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true) // this security configuration applies to entire app configuration
@@ -17,11 +21,21 @@ public class SecurityConfiguration {
         this.myUserDetailsService = myUserDetailsService;
     }
 
+    /**
+     * authJwtRequestFilter creates a new instance of the JWTRequestFilter,
+     * so we can validate the jwt token.
+     * @return a new JWTRequestFilter instance
+     */
     @Bean
     public JWTRequestFilter authJwtRequestFilter(){
         return new JWTRequestFilter();
     }
 
+    /**
+     * passwordEncoder creates a new instance of the BCryptPasswordEncoder,
+     * so we can salt and hash the user's password.
+     * @return a new BCryptPasswordEncoder instance
+     */
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
