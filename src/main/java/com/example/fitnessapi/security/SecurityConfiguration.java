2d156f298cce9d928cbab7bb07f2b732c0e0df3a
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:51:29 -0500

    create bean instance of JWTRequestFilter

--- src/main/java/com/example/fitnessapi/security/SecurityConfiguration.java
+++ src/main/java/com/example/fitnessapi/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.fitnessapi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true) // this security configuration applies to entire app configuration
@@ -13,5 +15,10 @@ public class SecurityConfiguration {
     @Autowired
     public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
         this.myUserDetailsService = myUserDetailsService;
+    }
+
+    @Bean
+    public JWTRequestFilter authJwtRequestFilter(){
+        return new JWTRequestFilter();
     }
 }
