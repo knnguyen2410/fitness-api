3a8c94bd3a3bea507d37a6b40c74f71304c55706
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 02:15:35 -0500

    create method to authenticate user login

--- src/main/java/com/example/fitnessapi/service/UserService.java
+++ src/main/java/com/example/fitnessapi/service/UserService.java
@@ -2,12 +2,18 @@ package com.example.fitnessapi.service;
 
 import com.example.fitnessapi.exception.InformationExistException;
 import com.example.fitnessapi.model.User;
+import com.example.fitnessapi.model.request.LoginRequest;
+import com.example.fitnessapi.model.response.LoginResponse;
 import com.example.fitnessapi.repository.UserRepository;
 import com.example.fitnessapi.security.JWTUtils;
 import com.example.fitnessapi.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -35,7 +41,7 @@ public class UserService {
 
     // As a user, I can register for an account using my email address, and set a username and password.
     // http://localhost:9092/auth/users/register/
-    
+
     /**
      * createUser takes in a user object and creates a new user.
      * If the email address in the user object already belongs to a user,
@@ -64,5 +70,24 @@ public class UserService {
     // As a user, I can log into a website using my email and password
     // http://localhost:9092/auth/users/login/
 
-
+    /**
+     * ResponseEntity returns a key-value pair (? means object).
+     * The loginUser method authenticates the user, grabs user details,
+     * generates token, and sends it to our application.
+     * @param loginRequest email and password used to autheticate user
+     * @return jwt or error message
+     */
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            myUserDetails = (MyUserDetails) authentication.getPrincipal(); // now we know who you are
+            // generate token
+            final String JWT = jwtUtils.generateJwtToken(myUserDetails);
+            return ResponseEntity.ok(new LoginResponse(JWT)); // ok means everything went fine
+        } catch (Exception e) {
+            return ResponseEntity.ok(new LoginResponse("Error: username or password is incorrect."));
+        }
+    }
 }
