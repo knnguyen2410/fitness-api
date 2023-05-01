952fea2500d4c9af4f9b393bca260ccc0b26e974
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:30:45 -0500

    add logger

--- src/main/java/com/example/fitnessapi/security/JWTRequestFilter.java
+++ src/main/java/com/example/fitnessapi/security/JWTRequestFilter.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapi.security;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -7,8 +8,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
+@Component
 public class JWTRequestFilter extends OncePerRequestFilter {
+
+    // logger allows us to save information in server's memory, but not hard drive
+    Logger logger = Logger.getLogger(JWTRequestFilter.class.getName());
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
