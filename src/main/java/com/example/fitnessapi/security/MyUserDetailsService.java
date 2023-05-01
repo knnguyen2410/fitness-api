54a4e4f9a997a9025ef46b892a36ec2bb270a8e9
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:08:42 -0500

    instantiate user service

--- src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
@@ -1,4 +1,16 @@
 package com.example.fitnessapi.security;
 
+import com.example.fitnessapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MyUserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
