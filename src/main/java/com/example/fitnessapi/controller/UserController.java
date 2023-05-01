e513307e9cd0c97c0401a6d7f826fb486982eb46
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 23:38:42 -0500

    instantiate user service in user controller

--- src/main/java/com/example/fitnessapi/controller/UserController.java
+++ src/main/java/com/example/fitnessapi/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.fitnessapi.controller;
 
+import com.example.fitnessapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/auth/users/") // this is the designated url path: http://localhost:9092/auth/users/
 public class UserController {
 
-// http://localhost:9092/auth/users/register/
+    public UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // http://localhost:9092/auth/users/register/
 // As a user, I can register for an account using my email address, and set a username and password.
 // http://localhost:9092/auth/users/login/
 // As a user, I can log into a website using my username and password
