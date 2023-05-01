0b55af7624714ab78a64b4443a387c51dfe75ffc
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:39:58 -0500

    implement the UserDetailsService interface and update the method to load user by username

--- src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
+++ src/main/java/com/example/fitnessapi/security/MyUserDetailsService.java
@@ -1,16 +1,26 @@
 package com.example.fitnessapi.security;
 
+import com.example.fitnessapi.model.User;
 import com.example.fitnessapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyUserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
     private UserService userService;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
     }
 }
