4dfe5b0fc947bc525ba779f66eac6484ca2b0af5
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 00:54:06 -0500

    instantiate workout repository in workout service

--- src/main/java/com/example/fitnessapi/controller/WorkoutController.java
+++ src/main/java/com/example/fitnessapi/controller/WorkoutController.java
@@ -2,8 +2,7 @@ package com.example.fitnessapi.controller;
 
 import com.example.fitnessapi.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // this is a rest controller for our rest api
 @RequestMapping(path = "/api/") // this is the designated url path: http://localhost:9092/api/
@@ -15,16 +14,4 @@ public class WorkoutController {
     public void setWorkoutService(WorkoutService workoutService) {
         this.workoutService = workoutService;
     }
-
-    // http://localhost:9092/api/workouts/
-// (POST) As a user, I can create a new workout
-// http://localhost:9092/api/workouts/
-// (GET) As a user, I can see a list of all my workouts
-// http://localhost:9092/api/workouts/{workoutId}/
-// (GET) As a user, I can see a certain workout
-// http://localhost:9092/api/workouts/{workoutId}/
-// (PUT) As a user, I can update a certain category
-// http://localhost:9092/api/workouts/{workoutId}/
-// (DELETE) As a user, I can delete a certain workout
-
 }
--- src/main/java/com/example/fitnessapi/service/WorkoutService.java
+++ src/main/java/com/example/fitnessapi/service/WorkoutService.java
@@ -1,26 +1,32 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkoutService {
 
-    private WorkoutService workoutService;
+    public WorkoutRepository workoutRepository;
 
     @Autowired
-    public void setWorkoutService(WorkoutService workoutService) {
-        this.workoutService = workoutService;
+    public void setWorkoutRepository(WorkoutRepository workoutRepository) {
+        this.workoutRepository = workoutRepository;
     }
 
-// http://localhost:9092/api/workouts/
-// (POST) As a user, I can create a new workout
-// http://localhost:9092/api/workouts/
-// (GET) As a user, I can see a list of all my workouts
-// http://localhost:9092/api/workouts/{workoutId}/
-// (GET) As a user, I can see a certain workout
-// http://localhost:9092/api/workouts/{workoutId}/
-// (PUT) As a user, I can update a certain category
-// http://localhost:9092/api/workouts/{workoutId}/
-// (DELETE) As a user, I can delete a certain workout
+    // (POST) As a user, I can create a new workout
+    // http://localhost:9092/api/workouts/
+
+    // (GET) As a user, I can see a list of all my workouts
+    // http://localhost:9092/api/workouts/
+
+    // (GET) As a user, I can see a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
+
+    // (PUT) As a user, I can update a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
+
+    // (DELETE) As a user, I can delete a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
+
 }
