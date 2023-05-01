37bf7e30eb4a5ff3f00df0b74a1ad1d2c98b3738
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 23:01:27 -0500

    instantiate ExerciseRepository

--- src/main/java/com/example/fitnessapi/service/ExerciseService.java
+++ src/main/java/com/example/fitnessapi/service/ExerciseService.java
@@ -1,4 +1,25 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ExerciseService {
+    private ExerciseRepository exerciseRepository;
+
+    @Autowired
+    public void setExerciseRepository(ExerciseRepository exerciseRepository) {
+        this.exerciseRepository = exerciseRepository;
+    }
+    
+// http://localhost:9092/api/workouts/{workoutId}/exercises/
+// (POST) As a user, I can create a new exercise in a certain workout
+// http://localhost:9092/api/workouts/{workoutId}/exercises/
+// (GET) As a user, I can get a list of all my exercises for a certain workout
+// http://localhost:9092/api/workouts/{workoutId}/exercises/{exerciseId}
+// (GET) As a user, I can get a certain exercise for a certain workout
+// http://localhost:9092/api/workouts/{workoutId}/exercises/{exerciseId}
+// (DELETE) As a user, I can delete a certain exercise for a certain workout
+
 }
