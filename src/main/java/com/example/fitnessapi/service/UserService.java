2ce21a9c183927609e616caed9aeb417ed25a60c
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 22:26:34 -0500

    instantiate UserRepository

--- src/main/java/com/example/fitnessapi/service/UserService.java
+++ src/main/java/com/example/fitnessapi/service/UserService.java
@@ -1,4 +1,23 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+// http://localhost:9092/auth/users/register/
+// As a user, I can register for an account using my email address, and set a username and password.
+// http://localhost:9092/auth/users/login/
+// As a user, I can log into a website using my username and password
+
+
 }
