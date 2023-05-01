f82f26a40f0d82855f113bace6c920fa8960c7ae
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 03:35:08 -0500

    create new exercise for a workout

--- src/main/java/com/example/fitnessapi/controller/ExerciseController.java
+++ src/main/java/com/example/fitnessapi/controller/ExerciseController.java
@@ -1,9 +1,10 @@
 package com.example.fitnessapi.controller;
 
+import com.example.fitnessapi.model.Exercise;
 import com.example.fitnessapi.service.ExerciseService;
+import com.example.fitnessapi.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController // this is a rest controller for our rest api
@@ -11,14 +12,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExerciseController {
 
     public ExerciseService exerciseService;
+    public WorkoutService workoutService;
 
     @Autowired
     public void setExerciseService(ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
 
+    @Autowired
+    public void setWorkoutService(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
+
+    /**
+     * createWorkoutExercise uses the createWorkoutExercise from the exerciseService
+     * to create a new workout for a certain exercise.
+     * @param workoutId is the workout we want to make a new exercise in
+     * @param exerciseObject is the exercise we want to make
+     * @return calls upon the createWorkoutExercise in the exerciseService
+     */
     // (POST) As a user, I can create a new exercise for a certain workout
     // http://localhost:9092/api/workouts/{workoutId}/exercises/
+    @PostMapping(path = "/workouts/{workoutId}/exercises/")
+    public Exercise createWorkoutExercise(@PathVariable Long workoutId, @RequestBody Exercise exerciseObject){
+        return exerciseService.createWorkoutExercise(workoutId, exerciseObject);
+    }
 
     // (GET) As a user, I can get a list of all my exercises for a certain workout
     // http://localhost:9092/api/workouts/{workoutId}/exercises/
--- src/main/java/com/example/fitnessapi/service/ExerciseService.java
+++ src/main/java/com/example/fitnessapi/service/ExerciseService.java
@@ -1,20 +1,55 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.exception.InformationExistException;
+import com.example.fitnessapi.exception.InformationNotFoundException;
+import com.example.fitnessapi.model.Exercise;
+import com.example.fitnessapi.model.Workout;
 import com.example.fitnessapi.repository.ExerciseRepository;
+import com.example.fitnessapi.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class ExerciseService {
+
     private ExerciseRepository exerciseRepository;
+    private WorkoutRepository workoutRepository;
 
     @Autowired
     public void setExerciseRepository(ExerciseRepository exerciseRepository) {
         this.exerciseRepository = exerciseRepository;
     }
 
+    @Autowired
+    public void setWorkoutRepository(WorkoutRepository workoutRepository) {
+        this.workoutRepository = workoutRepository;
+    }
+
+    /**
+     * createWorkoutExercise creates a new exercise for a certain workout id.
+     * If the workout doesn't exist, or the exercise already exists, throw an exception
+     * @param workoutId is the workout we want to make a new exercise in
+     * @param exerciseObject is the exercise we want to make
+     * @return the new exercise object
+     */
     // (POST) As a user, I can create a new exercise for a certain workout
     // http://localhost:9092/api/workouts/{workoutId}/exercises/
+    public Exercise createWorkoutExercise(Long workoutId, Exercise exerciseObject) {
+        Workout workout = workoutRepository.findByIdAndUserId(workoutId, WorkoutService.getCurrentLoggedInUser().getId());
+        if (workout == null) {
+            throw new InformationNotFoundException(
+                    "Workout with id " + workoutId + " does not belong to this user or workout does not exist.");
+        }
+        Exercise exercise = exerciseRepository.findByUserIdAndName(WorkoutService.getCurrentLoggedInUser().getId(), exerciseObject.getName());
+        if (exercise != null) {
+            throw new InformationExistException("Exercise with name " + exercise.getName() + " already exists.");
+        }
+        exerciseObject.setUser(WorkoutService.getCurrentLoggedInUser());
+        exerciseObject.setWorkoutList(Arrays.asList(workout));
+        return exerciseRepository.save(exerciseObject);
+    }
 
     // (GET) As a user, I can get a list of all my exercises for a certain workout
     // http://localhost:9092/api/workouts/{workoutId}/exercises/
