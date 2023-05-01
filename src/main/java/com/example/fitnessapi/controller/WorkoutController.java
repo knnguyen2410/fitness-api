d7a902ca2de22e4d271184e402b7df77bd08aefb
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 02:24:55 -0500

    create workout

--- src/main/java/com/example/fitnessapi/controller/WorkoutController.java
+++ src/main/java/com/example/fitnessapi/controller/WorkoutController.java
@@ -1,5 +1,7 @@
 package com.example.fitnessapi.controller;
 
+import com.example.fitnessapi.exception.InformationExistException;
+import com.example.fitnessapi.model.Workout;
 import com.example.fitnessapi.service.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,4 +16,23 @@ public class WorkoutController {
     public void setWorkoutService(WorkoutService workoutService) {
         this.workoutService = workoutService;
     }
+
+    // (POST) As a user, I can create a new workout
+    // http://localhost:9092/api/workouts/
+    @PostMapping(path = "/workouts/")
+    public Workout createWorkout(@RequestBody Workout workoutObject){
+        return workoutService.createWorkout(workoutObject);
+    }
+    
+    // (GET) As a user, I can see a list of all my workouts
+    // http://localhost:9092/api/workouts/
+
+    // (GET) As a user, I can see a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
+
+    // (PUT) As a user, I can update a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
+
+    // (DELETE) As a user, I can delete a certain workout
+    // http://localhost:9092/api/workouts/{workoutId}/
 }
--- src/main/java/com/example/fitnessapi/service/WorkoutService.java
+++ src/main/java/com/example/fitnessapi/service/WorkoutService.java
@@ -1,6 +1,8 @@
 package com.example.fitnessapi.service;
 
+import com.example.fitnessapi.exception.InformationExistException;
 import com.example.fitnessapi.model.User;
+import com.example.fitnessapi.model.Workout;
 import com.example.fitnessapi.repository.WorkoutRepository;
 import com.example.fitnessapi.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,15 @@ public class WorkoutService {
 
     // (POST) As a user, I can create a new workout
     // http://localhost:9092/api/workouts/
+    public Workout createWorkout(Workout workoutObject){
+        Workout workout = workoutRepository.findByUserIdAndName(WorkoutService.getCurrentLoggedInUser().getId(), workoutObject.getName());
+        if (workout != null) {
+            throw new InformationExistException("Workout with the name " + workoutObject.getName() + " already exists.");
+        } else {
+            workoutObject.setUser(getCurrentLoggedInUser());
+            return workoutRepository.save(workoutObject);
+        }
+    }
 
     // (GET) As a user, I can see a list of all my workouts
     // http://localhost:9092/api/workouts/
