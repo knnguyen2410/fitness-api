777a8a54fb8ef8f7a3e5ed25a51306bd9936974a
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:05:47 -0500

    add user instance

--- src/main/java/com/example/fitnessapi/security/MyUserDetails.java
+++ src/main/java/com/example/fitnessapi/security/MyUserDetails.java
@@ -1,11 +1,27 @@
 package com.example.fitnessapi.security;
 
+import com.example.fitnessapi.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
+
+    // create instance of user
+    // private means not accessible by other classes
+    // final means user value cannot be changed
+    private final User user;
+
+    // constructor
+    public MyUserDetails(User user){
+        this.user = user;
+    }
+
+    // getter only, since we're not setting the user
+    public User getUser(){
+        return user;
+    }
 
     // methods implemented from UserDetails interface
     @Override
