ccd560f8e9e5819160c884155b0d1a6547e97094
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-04-30 22:33:32 -0500

    designate the WorkoutController class as a rest controller

--- src/main/java/com/example/fitnessapi/controller/WorkoutController.java
+++ src/main/java/com/example/fitnessapi/controller/WorkoutController.java
@@ -1,4 +1,21 @@
 package com.example.fitnessapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // this is a rest controller for our rest api
+@RequestMapping(path = "/api/") // this is the designated url path: http://localhost:9092/api/
 public class WorkoutController {
+
+// http://localhost:9092/api/workouts/
+// (POST) As a user, I can create a new workout
+// http://localhost:9092/api/workouts/
+// (GET) As a user, I can see a list of all my workouts
+// http://localhost:9092/api/workouts/{workoutId}/
+// (GET) As a user, I can see a certain workout
+// http://localhost:9092/api/workouts/{workoutId}/
+// (PUT) As a user, I can update a certain category
+// http://localhost:9092/api/workouts/{workoutId}/
+// (DELETE) As a user, I can delete a certain workout
+
 }
