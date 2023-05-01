7bf6f55e7dd137d58a54034fa53777157afc083c
Kim Nguyen <kimnguyen@Kims-Air.cable.rcn.com>
2023-05-01 01:14:18 -0500

    rearrange comments

--- src/main/java/com/example/fitnessapi/service/UserService.java
+++ src/main/java/com/example/fitnessapi/service/UserService.java
@@ -1,7 +1,5 @@
 package com.example.fitnessapi.service;
 
-import com.example.fitnessapi.exception.InformationExistException;
-import com.example.fitnessapi.model.User;
 import com.example.fitnessapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +14,10 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-// http://localhost:9092/auth/users/register/
 // As a user, I can register for an account using my email address, and set a username and password.
-// http://localhost:9092/auth/users/login/
+// http://localhost:9092/auth/users/register/
+
 // As a user, I can log into a website using my username and password
-
-
+// http://localhost:9092/auth/users/login/
+    
 }
